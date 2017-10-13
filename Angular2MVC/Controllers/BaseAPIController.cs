88f1bdd0ae4c3ddbb56d8a240b45458e56bf9315
Sharipov <rsharipov@medline.com>
2017-10-13 17:44:16 -0500

    Add project files.

---
+++ Angular2MVC/Controllers/BaseAPIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Angular2MVC.DBContext;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Angular2MVC.Controllers
+{
+    public class BaseAPIController : ApiController
+    {
+        protected readonly UserDBEntities UserDB = new UserDBEntities();
+        protected HttpResponseMessage ToJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+
+        protected HttpResponseMessage ErrorJson(dynamic obj)
+        {
+            var response = Request.CreateResponse(HttpStatusCode.Unauthorized);
+            response.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
